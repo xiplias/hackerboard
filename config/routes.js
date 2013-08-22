368490679ceb198050a251cc58f15ad2e1106049
Anders Hansen <xiplias@gmail.com>
2013-08-23 00:11:53 +0200

    More removal of old routes

--- config/routes.js
+++ config/routes.js
@@ -6,12 +6,8 @@ module.exports = function (app, passport, auth) {
   // user routes
   var users = require('../app/controllers/users')
   app.get('/login', users.signin)
-  app.get('/signup', users.signup)
   app.get('/logout', users.signout)
-  app.post('/users', users.create)
-  app.post('/users/session', passport.authenticate('local', {failureRedirect: '/signin', failureFlash: 'Invalid email or password.'}), users.session)
-  app.get('/users/me', users.me)
-  app.get('/users/:userId', users.show)
+
   app.get('/auth/github', passport.authenticate('github', { failureRedirect: '/signin' }), users.signin)
   app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/signin' }), users.authCallback)
 
