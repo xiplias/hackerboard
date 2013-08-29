842606dd82373f911431a38411b8b7aa43fdf9e0
Anders Hansen <xiplias@gmail.com>
2013-08-29 14:42:12 +0200

    Changing the config file

--- config/config.js
+++ config/config.js
@@ -1,42 +1,18 @@
 
-var path = require('path')
-  , rootPath = path.normalize(__dirname + '/..')
+var path      = require('path'),
+    rootPath  = path.normalize(__dirname + '/..'); 
 
 module.exports = {
-  development: {
-    db: 'mongodb://localhost/hackerboard-dev',
-    root: rootPath,
-    app: {
-      name: 'Copenhagen Node.js Hackathon - Development'
-    },
-    github: {
-      clientID: '77c4b42a63e4ee524d17',
-      clientSecret: '2def3795fdc3a5924dacef30ad84728c5bd6449c',
-      callbackURL: 'http://localhost:3000/auth/github/callback'
-    },
-  },
-  test: {
-    db: 'mongodb://localhost/hackerboard-test',
-    root: rootPath,
-    app: {
-      name: 'Copenhagen Node.js Hackathon - Test'
-    },
-    github: {
-      clientID: 'APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/auth/github/callback'
-    },
-  },
   production: {
-    db: 'mongodb://localhost/hackerboard',
+    db: process.env.MONGO_URL,
     root: rootPath,
     app: {
       name: 'Copenhagen Node.js Hackathon'
     },
     github: {
-      clientID: 'APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/auth/github/callback'
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK
     }
   }
-}
+};
